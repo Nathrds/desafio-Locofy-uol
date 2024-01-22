170327ba2e26fe47a291db4b750d5855ca4f5b6f
Nathália <nathaliarsantos@live.com>
2024-01-22 20:28:02 -0300

    Footer content with styling is ready

--- desafio-locofy/src/footer/Footer.tsx
+++ desafio-locofy/src/footer/Footer.tsx
@@ -1,8 +1,155 @@
-import React from 'react'
+import { Box, Container, IconButton, Typography } from "@mui/material"
+
+const useStyles = {
+    logoFooter: {
+        maxWidth: '100%'
+    }
+}
 
 const Footer: React.FC = () => {
   return (
-    <div>Footer</div>
+    <Box 
+    margin={'40px 80px'} 
+    display='flex'
+    flexDirection={'row'}
+    justifyContent={'space-between'}
+    >
+        <Box>
+            <img src="../../public/Logo-white.png" alt="Logo My Ride" style={useStyles.logoFooter}/>
+            <Typography
+            sx={{
+                color: '#B3B3B3',
+                fontSize: '16px',
+                fontWeight: '400',
+                margin: '16px 0'
+            }}
+            >
+                MyRide Inc., 2nd Floor, New York, NY 10016
+            </Typography>
+            <Box>
+                <IconButton sx={{padding: 0}}>
+                    <img src="../../public/Facebook.png" alt="Icon Facebook" />
+                </IconButton>
+                <IconButton sx={{margin: '0 24.38px', padding: 0}}>
+                    <img src="../../public/Instagram.png" alt="Icon Instagram" />
+                </IconButton>
+                <IconButton sx={{padding: 0}}>
+                    <img src="../../public/Twitter.png" alt="Icon Twitter" />
+                </IconButton>
+            </Box>
+        </Box>
+        <Container sx={{display: "flex", flexDirection:"row"}}>
+            <Box marginRight={'20%'}>
+                <Typography 
+                variant="h5"
+                color="#fff"
+                fontSize='20px'
+                fontWeight={500}
+                marginBottom={'20.76px'}
+                >
+                    Company
+                </Typography>
+                <Box 
+                display="flex" 
+                flexDirection={"column"}
+                color="#fff"
+                fontSize='16px'
+                fontWeight={400}
+                >
+                    <Typography
+                    marginBottom={'12px'}
+                    >
+                        About Us
+                    </Typography>
+                    <Typography
+                    marginBottom={'12px'}
+                    >
+                        News
+                    </Typography>
+                    <Typography
+                    marginBottom={'12px'}
+                    >
+                        Careers
+                    </Typography>
+                    <Typography
+                    >
+                        How we work
+                    </Typography>
+                </Box>
+            </Box>
+            <Box marginRight={'20%'}>
+                <Typography 
+                variant="h5"
+                color="#fff"
+                fontSize='20px'
+                fontWeight={500}
+                marginBottom={'20.76px'}
+                >
+                    Support
+                </Typography>
+                <Box 
+                display="flex" 
+                flexDirection={"column"}
+                color="#fff"
+                fontSize='16px'
+                fontWeight={400}
+                >
+                    <Typography
+                    marginBottom={'12px'}
+                    >
+                        FAQ
+                    </Typography>
+                    <Typography
+                    marginBottom={'12px'}
+                    >
+                        US Office
+                    </Typography>
+                    <Typography
+                    marginBottom={'12px'}
+                    >
+                        Asia Office
+                    </Typography>
+                    <Typography
+                    >
+                        Help Center
+                    </Typography>
+                </Box>
+            </Box>
+            <Box marginRight={'20%'}>
+                <Typography 
+                variant="h5"
+                color="#fff"
+                fontSize='20px'
+                fontWeight={500}
+                marginBottom={'20.76px'}
+                >
+                    More
+                </Typography>
+                <Box 
+                display="flex" 
+                flexDirection={"column"}
+                color="#fff"
+                fontSize='16px'
+                fontWeight={400}
+                >
+                    <Typography
+                    marginBottom={'12px'}
+                    >
+                        Become a partner
+                    </Typography>
+                    <Typography
+                    marginBottom={'12px'}
+                    >
+                        Partner Support
+                    </Typography>
+                    <Typography
+                    >
+                        Mobile app links
+                    </Typography>
+                </Box>
+            </Box>
+        </Container>
+    </Box>
   )
 }
 
