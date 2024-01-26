9f15d73e3c1c2a26a77d445f10e621e604d5e24d
Nathália <nathaliarsantos@live.com>
2024-01-26 15:01:45 -0300

    Fixed the styling of the success screen

--- desafio-locofy/src/driveWithMyRide/successScreen/SuccessScreen.tsx
+++ desafio-locofy/src/driveWithMyRide/successScreen/SuccessScreen.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Typography } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { useEffect, useState } from "react";
-import ModalError from "../modal/Modal"
+import ModalError from "../modal/Modal";
 
 interface SuccessScreenProps {
   onClick: () => void;
@@ -17,7 +17,7 @@ const SuccessScreen: React.FC<SuccessScreenProps> = ({ onClick }) => {
       async function fetchData() {
         const response = await fetch("http://localhost:3000/cars/1");
         if (!response.ok) {
-          setError(true)
+          setError(true);
         }
         const data = await response.json();
         return data;
@@ -60,107 +60,164 @@ const SuccessScreen: React.FC<SuccessScreenProps> = ({ onClick }) => {
         <>
           <Box
             display="flex"
+            flexDirection="column"
             alignItems="center"
-            justifyContent="center"
             marginBottom="3%"
           >
-            <CheckCircleIcon style={{ color: "#4baf4f", fontSize: "50" }} />
-            <Typography
-              variant="h6"
-              sx={{
-                color: "#fff",
-                fontSize: "45px",
-                fontWeight: "700",
-              }}
-            >
-              Welcome, {data.fullName.split(" ")[0]}
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <CheckCircleIcon style={{ color: "#4baf4f", fontSize: "50" }} />
+              <Typography
+                variant="h4"
+                sx={{
+                  color: "#fff",
+                  fontSize: "45px",
+                  fontWeight: "700",
+                }}
+              >
+                Welcome, {data.fullName.split(" ")[0]}
+              </Typography>
+            </Box>
+            <Typography variant="h6" color={"#fff"} marginTop="2%">
+              Great, successfully submitted!
             </Typography>
           </Box>
 
-          <Typography
-            variant="body2"
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-around",
-              alignItems: "center",
-            }}
-          >
-            <Box display="flex" flexDirection="column">
-              <Typography
-                variant="body2"
-                color="#fff"
-                fontSize="16px"
-                margin="4% 0"
-              >
-                <span style={{ fontStyle: "italic", color: "#FBA403" }}>
-                  Full Name:
-                </span>{" "}
-                {data.fullName}
-              </Typography>
-              <Typography
-                variant="body2"
-                color="#fff"
-                fontSize="16px"
-                margin="4% 0"
-              >
-                <span style={{ fontStyle: "italic", color: "#FBA403" }}>
-                  Email Address:
-                </span>{" "}
-                {data.emailAddress}
-              </Typography>
-            </Box>
+          <Box display="flex" flexDirection="column" marginBottom="3%">
+            <Typography
+              variant="body2"
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-around",
+                alignItems: "center",
+              }}
+            >
+              <Box display="flex" flexDirection="column" marginRight="3%">
+                <Typography
+                  variant="body2"
+                  color="#fff"
+                  fontSize="16px"
+                  margin="5% 0"
+                >
+                  <span
+                    style={{
+                      fontStyle: "italic",
+                      color: "#FBA403",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    Full Name:
+                  </span>{" "}
+                  {data.fullName}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="#fff"
+                  fontSize="16px"
+                  margin="5% 0"
+                >
+                  <span
+                    style={{
+                      fontStyle: "italic",
+                      color: "#FBA403",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    Email Address:
+                  </span>{" "}
+                  {data.emailAddress}
+                </Typography>
+              </Box>
 
-            <Box display="flex" flexDirection="column">
-              <Typography
-                variant="body2"
-                color="#fff"
-                fontSize="16px"
-                margin="6% 0"
-              >
-                <span style={{ fontStyle: "italic", color: "#FBA403" }}>
-                  Country:
-                </span>{" "}
-                {data.country}
-              </Typography>
-              <Typography
-                variant="body2"
-                color="#fff"
-                fontSize="16px"
-                margin="6% 0"
-              >
-                <span style={{ fontStyle: "italic", color: "#FBA403" }}>
-                  City:
-                </span>{" "}
-                {data.city}
-              </Typography>
-            </Box>
+              <Box display="flex" flexDirection="column" marginRight="3%">
+                <Typography
+                  variant="body2"
+                  color="#fff"
+                  fontSize="16px"
+                  margin="8% 0"
+                >
+                  <span
+                    style={{
+                      fontStyle: "italic",
+                      color: "#FBA403",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    Country:
+                  </span>{" "}
+                  {data.country}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="#fff"
+                  fontSize="16px"
+                  margin="15% 0"
+                >
+                  <span
+                    style={{
+                      fontStyle: "italic",
+                      color: "#FBA403",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    City:
+                  </span>{" "}
+                  {data.city}
+                </Typography>
+              </Box>
 
-            <Box display="flex" flexDirection="column">
-              <Typography
-                variant="body2"
-                color="#fff"
-                fontSize="16px"
-                margin="4% 0"
-              >
-                <span style={{ fontStyle: "italic", color: "#FBA403" }}>
-                  Referral Code:
-                </span>{" "}
-                {data.referralCode}
-              </Typography>
-              <Typography
-                variant="body2"
-                color="#fff"
-                fontSize="16px"
-                margin="4% 0"
-              >
-                <span style={{ fontStyle: "italic", color: "#FBA403" }}>
-                  Car Type:
-                </span>{" "}
-                {data.carType}
-              </Typography>
-            </Box>
-          </Typography>
+              <Box display="flex" flexDirection="column">
+                <Typography
+                  variant="body2"
+                  color="#fff"
+                  fontSize="16px"
+                  margin="5% 0"
+                  width={"120px"}
+                >
+                  <span
+                    style={{
+                      fontStyle: "italic",
+                      color: "#FBA403",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    Referral Code:
+                  </span>{" "}
+                  {data.referralCode}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="#fff"
+                  fontSize="16px"
+                  margin="5% 0"
+                >
+                  <span
+                    style={{
+                      fontStyle: "italic",
+                      color: "#FBA403",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    Car Type:
+                  </span>{" "}
+                  {data.carType}
+                </Typography>
+              </Box>
+              <Box>
+                <img
+                  src="../../src/assets/Popup-image-car.png"
+                  alt="Well Done"
+                  style={{ maxWidth: "100%" }}
+                />
+              </Box>
+            </Typography>
+          </Box>
 
           <Box display="flex" justifyContent="center">
             <Button
